490c313b0d751f6426fa852773c6a72657052ba1
Tomasz Klim <github@tomaszklim.pl>
2019-06-16 10:25:28 +0200

    tolerate Lynis FIRE-4512 in case if no firewall rules are configured

--- cron/check.sh
+++ cron/check.sh
@@ -5,8 +5,15 @@ cd /opt/lynis
 /opt/lynis/lynis audit system --cronjob --quiet --profile serverfarmer.prf 2>/dev/null |grep -v custom.prf
 
 # 2. warn about obvious problems by email from crontab
-grep warning /var/log/lynis-report.dat \
-	|grep -vFf /opt/farm/ext/secure-lynis/config/allowed.conf \
-	|grep -vFf /etc/local/.config/allowed.lynis
+if [ ! -d /opt/farm/ext/firewall ] || [ -f /etc/local/.config/logcheck.nofirewall ]; then
+	grep warning /var/log/lynis-report.dat \
+		|grep -vFf /opt/farm/ext/secure-lynis/config/allowed.conf \
+		|grep -vFf /etc/local/.config/allowed.lynis |
+		|grep -v FIRE-4512
+else
+	grep warning /var/log/lynis-report.dat \
+		|grep -vFf /opt/farm/ext/secure-lynis/config/allowed.conf \
+		|grep -vFf /etc/local/.config/allowed.lynis
+fi
 
 # 3. leave /var/log/lynis.* files for possible inspection
