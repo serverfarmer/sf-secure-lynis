196e330b1a8b989eeff5546fa6bfbcd16fe4d2f1
Tomasz Klim <github@tomaszklim.pl>
2020-01-17 14:09:21 +0100

    syntax fix

--- internal/read-lynis-warnings.sh
+++ internal/read-lynis-warnings.sh
@@ -2,4 +2,4 @@
 
 grep warning /var/log/lynis-report.dat \
 	|grep -vFf /opt/farm/ext/secure-lynis/config/allowed.conf \
-	|grep -vFf /etc/local/.config/allowed.lynis \
+	|grep -vFf /etc/local/.config/allowed.lynis
